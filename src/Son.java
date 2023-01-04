59d12035582e9df668decd49a8b0e2b06c2467dd
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-04 11:43:49 +0300

    Update Son.java

--- src/Son.java
+++ src/Son.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Son {
+public final class Son extends Father{
+
+private String school;
+
+private String typeOfComputer;
+
+    public String getSchool() {
+        return school;
+    }
+
+    public Son(String name, House house, int age, Family family,String school, String typeOfComputer) {
+        super(house, name, age, family);
+        this.school = school;
+        this.typeOfComputer = typeOfComputer;
+    }
+
+    public String getTypeOfComputer() {
+        return typeOfComputer;
+
+    }
+
+    @Override
+    public void makeWork() {
+        System.out.println("Играет игры");
+    }
+
+    @Override
+    public String getInfo() {
+        return super.getInfo() + "school" + school +
+                "\ntypeOfComputer" + typeOfComputer;
+    }
 }
