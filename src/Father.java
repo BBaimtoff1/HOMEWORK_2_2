b866b8d4227ae25b78420e26cf4db6ca0e2a137e
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-04 11:41:58 +0300

    Update Father.java

--- src/Father.java
+++ src/Father.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Father {
+public class Father extends GrandFather {
+private int age;
+
+private Family family;
+
+    public Family getFamily() {
+        return family;
+    }
+
+    public Father(House house, String name, int age, Family family) {
+        super(house, name);
+        this.age = age;
+        this.family = family;
+    }
+
+    public int getAge() {
+        return age;
+
+    }
+    public void makeWork(){
+        System.out.println("Читает книгу");
+    }
+    final public void makeWork(String work){
+        System.out.println(work);
+    }
+
+    public String getInfo(){
+        return "name " + getName() +
+                "\nHouse" + getHouse() +
+                "\nAge" + age +
+                "\nFamily" + family.getName() + family.getAge();
+    }
 }
+
+
