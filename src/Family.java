8c692d38c87c63704f4e7fc234cc9cd7e7d811c4
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-04 11:41:27 +0300

    Update Family.java

--- src/Family.java
+++ src/Family.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Family {
+public class Family {
+    private int age;
+    private String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public Family(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
