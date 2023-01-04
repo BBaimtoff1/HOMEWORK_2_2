85798426462efcb40bebb35731d5291ac1a15c30
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-04 11:43:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Family family = new Family(38, "Alex");
+        Father father = new Father(House.AVERAGE, "Misha", 36, family);
+        father.makeWork();
+        father.makeWork("Пишет код");
+        System.out.println(father.getInfo());
+        Family family1 = new Family(45, "Sasha");
+        Son son = new Son("Alim", House.BIG, 15, family1, "Kadurov", "Acer");
+        son.makeWork();
+        son.makeWork("Играет игры");
+        System.out.println(son.getInfo());
+        Family family2 = new Family(50, "Nurs");
+        Son son1 = new Son("Adilet", House.SMALL, 18, family2, "Gim", "Windows");
+        son1.makeWork();
+        son1.makeWork("Работает");
+        System.out.println(son1.getInfo());
     }
+
 }
