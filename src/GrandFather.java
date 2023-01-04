e5541995182a2621ab951c94630a00f6d328ec02
BBaimtoff1 <120623006+BBaimtoff1@users.noreply.github.com>
2023-01-04 11:42:28 +0300

    Update GrandFather.java

--- src/GrandFather.java
+++ src/GrandFather.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class GrandFather {
+public class GrandFather {
+
+    public String getName() {
+        return name;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    private House house;
+    private String name;
+
+    public GrandFather(House house, String name) {
+        this.house = house;
+        this.name = name;
+    }
 }
